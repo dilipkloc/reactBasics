f3aaaf4ad2794397cec61c8e464f965c34ae775b
alucard-alexander <38307899+alucard-alexander@users.noreply.github.com>
2020-03-01 18:00:53 +0530

    feat: event handling

--- test/src/App.js
+++ test/src/App.js
@@ -8,11 +8,13 @@ export default class App extends Component{
     persons: [
       {name: 'dilip'},
       {name: 'kumar'}
-    ]
+    ],
+    isValues:false
   }
 
   clickPrintName = (e) => {
     //<h3 style={{textAlign: 'right'}}> By:  {this.state.persons[0].name} {this.state.persons[1].name}</h3>
+    this.setState({isValues:true})
     ReactDOM.render(<h3 style={{textAlign: 'right'}}> By:  {this.state.persons[0].name} {this.state.persons[1].name}</h3>,document.getElementById('by'))
     console.log('hit');
   }
@@ -26,7 +28,7 @@ export default class App extends Component{
           {this.props.children}
         </h2>
           <button onClick={this.clickPrintName}> Click here to know the author </button>
-          <div id="by"></div>
+          
       </div>
     )
   }
